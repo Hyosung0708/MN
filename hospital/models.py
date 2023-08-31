318ff58d038d528ba2707a86ecd25323c01bdd65
Shinhyosung <tlsgytjd0218@naver.com>
2023-08-31 15:20:32 +0900

    MN

---
+++ hospital/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=10)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=10)
+    doctors = models.ManyToManyField(Doctor, through='Reservation')
+
+
+class Reservation(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
